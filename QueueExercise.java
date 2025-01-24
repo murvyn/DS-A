1629a60c02871df55c4ca96b222456be6e592c36
Murvyn <marvin.asamoah.123@gmail.com>
2025-01-24 21:39:44 +0000

    implement priority queues

--- QueueExercise.java
+++ QueueExercise.java
@@ -1,16 +1,27 @@
 
+import java.lang.reflect.Array;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.Stack;
 
 public class QueueExercise {
     public static void main(String[] args) {
-        Queue<Integer> queue = new ArrayDeque<>();
+        // Queue<Integer> queue = new ArrayDeque<>();
+        // queue.add(10);
+        // queue.add(20);
+        // queue.add(30);
+        // reverse(queue);
+        // System.out.println(queue);
+        PriorityQueue queue = new PriorityQueue();
         queue.add(10);
-        queue.add(20);
-        queue.add(30);
-        reverse(queue);
-        System.out.println(queue);
+        queue.add(5);
+        queue.add(15);
+        queue.add(25);
+        queue.add(25);
+        queue.add(25);
+        queue.add(25);
+        System.out.println(queue.toString());
     }
 
     public static void reverse(Queue<Integer> queue) {
@@ -107,5 +118,50 @@ class StackQueue {
     }
 }
 
+class PriorityQueue {
+    private int[] items = new int[5];
+    private int count;
 
+    public boolean isEmpty() {
+        return count == 0;
+    }
 
+    public boolean isFull() {
+        return count == items.length;
+    }
+
+    public int remove() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Queue is empty");
+        }
+        return items[--count];
+    }
+
+    public void add(int item) {
+        if (isFull()) {
+            int newLength = items.length * 2;
+            int[] newItems = Arrays.copyOf(items, newLength);
+            items = newItems;
+        }
+        int i = shiftItemsToInsert(item);
+        items[i] = item;
+        count++;
+    }
+
+    private int shiftItemsToInsert(int item) {
+        int i;
+        for (i = count - 1; i >= 0; i--) {
+            if (items[i] > item) {
+                items[i + 1] = items[i];
+            } else {
+                break;
+            }
+        }
+        return i + 1;
+    }
+
+    @Override
+    public String toString() {
+        return Arrays.toString(items);
+    }
+}
