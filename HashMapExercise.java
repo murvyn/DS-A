20b74753bf0ccac8bcbdc9044bee6966b94780dd
Murvyn <marvin.asamoah.123@gmail.com>
2025-01-31 11:14:50 +0000

    hash table implementation

--- HashMapExercise.java
+++ HashMapExercise.java
@@ -1,6 +1,7 @@
 
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 
@@ -8,6 +9,12 @@ public class HashMapExercise {
     public static void main(String[] args) throws Exception {
         System.out.println(findFirstNonRepeatingChar("a green apple"));
         System.out.println(findFirstRepeatedChar("a green apple"));
+        
+        HashTable table = new HashTable();
+        table.put(6, "A");
+        table.put(8, "B");
+        table.put(11, "C");
+        System.out.println("done");
 
     }
 
@@ -34,5 +41,48 @@ public class HashMapExercise {
             set.add(ch);
         }
         return Character.MIN_VALUE;
+    }
+}
+
+class HashTable {
+    private class Entry {
+        private int key;
+        private String value;
+
+        public Entry(int key, String value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private LinkedList<Entry>[] entries = new LinkedList[5];
+
+    public void put(int key, String value) {
+        var index = hash(key);
+        if (entries[index] == null) {
+            entries[index] = new LinkedList<>();
+        }
+        for (var entry : entries[index]) {
+            if (entry.key == key) {
+                entry.value = value;
+                return;
+            }
+        }
+        entries[index].addLast(new Entry(key, value));
+    }
+
+    public String get(int key) {
+        var index = hash(key);
+        if (entries[index] != null) {
+            for (var entry : entries[index]) {
+                if (entry.key == key)
+                    return entry.value;
+            }
+        }
+        return null;
+    }
+
+    private int hash(int key) {
+        return key % entries.length;
     }
 }
