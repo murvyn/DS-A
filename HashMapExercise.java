07e0cf5e9e8fa752fd78581980deab8bfa299011
Murvyn <marvin.asamoah.123@gmail.com>
2025-02-01 23:25:09 +0000

    change size

--- HashMapExercise.java
+++ HashMapExercise.java
@@ -55,7 +55,7 @@ class HashTable {
         }
     }
 
-    private LinkedList<Entry>[] entries = new LinkedList[5];
+    private LinkedList<Entry>[] entries = new LinkedList[6];
 
     public void put(int key, String value) {
         var index = hash(key);
