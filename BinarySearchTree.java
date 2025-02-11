b139581e77f68ed6aefc32c966036772dc93a270
Murvyn <marvin.asamoah.123@gmail.com>
2025-02-11 16:46:12 +0000

    add height method

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,6 +1,6 @@
 
 public class BinarySearchTree {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Tree tree = new Tree();
         tree.insert(5);
         tree.insert(1);
@@ -24,22 +24,23 @@ class Tree {
         public Node(int value) {
             this.value = value;
         }
+
         @Override
         public String toString() {
             return "Node = " + value;
         }
     }
 
-
     private Node root;
 
     public boolean find(int value) {
         var current = root;
-        while(current != null) {
-            if(value == current.value) return true;
-            else if(value < current.value) {
+        while (current != null) {
+            if (value == current.value)
+                return true;
+            else if (value < current.value) {
                 current = current.leftChild;
-            }else if(value > current.value) {
+            } else if (value > current.value) {
                 current = current.rightChild;
             }
         }
@@ -48,59 +49,71 @@ class Tree {
 
     public void insert(int value) {
         Node node = new Node(value);
-        if(root == null) {
+        if (root == null) {
             root = node;
             return;
         }
         var current = root;
-        while (true) { 
-            if(value < current.value) {
-                if(current.leftChild == null){
+        while (true) {
+            if (value < current.value) {
+                if (current.leftChild == null) {
                     current.leftChild = node;
                     break;
                 }
                 current = current.leftChild;
-            }
-            else {
-                if(current.rightChild == null){
+            } else {
+                if (current.rightChild == null) {
                     current.rightChild = node;
                     break;
                 }
                 current = current.rightChild;
             }
-        }}
-       
- 
-        public void traversePreOrder () {
+        }
+    }
+
+    public void traversePreOrder() {
         traversePreOrder(root);
     }
 
-    private void traversePreOrder (Node root) {
-        if(root == null) return;
+    private void traversePreOrder(Node root) {
+        if (root == null)
+            return;
         System.out.println(root.value);
         traversePreOrder(root.leftChild);
         traversePreOrder(root.rightChild);
     }
-    
-    public void traverseInOrder () {
+
+    public void height() {
+        height(root);
+    }
+
+    private int height(Node root) {
+        if (root.leftChild == null && root.rightChild == null)
+            return 0;
+        return 1 + Math.max(height(root.leftChild), height(root.rightChild));
+    }
+
+    public void traverseInOrder() {
         traverseInOrder(root);
     }
 
-    private void traverseInOrder (Node root) {
-        if(root == null) return;
+    private void traverseInOrder(Node root) {
+        if (root == null)
+            return;
         traverseInOrder(root.leftChild);
         System.out.println(root.value);
         traverseInOrder(root.rightChild);
     }
-    
-    public void traversePostOrder () {
+
+    public void traversePostOrder() {
         traversePostOrder(root);
     }
 
-    private void traversePostOrder (Node root) {
-        if(root == null) return;
+    private void traversePostOrder(Node root) {
+        if (root == null)
+            return;
         traversePostOrder(root.leftChild);
         traversePostOrder(root.rightChild);
         System.out.println(root.value);
     }
-    }
+}
