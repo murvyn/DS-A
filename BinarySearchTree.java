f2125f99ae355bb91ae2e8203a2cf3a82baa8dc1
Murvyn <marvin.asamoah.123@gmail.com>
2025-02-11 17:13:29 +0000

    add min method

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -89,9 +89,25 @@ class Tree {
 
     private int height(Node root) {
         if (root == null) return -1;
-        if (root.leftChild == null && root.rightChild == null)
+        if (isLeaf(root))
             return 0;
         return 1 + Math.max(height(root.leftChild), height(root.rightChild));
+    }
+    public void min() {
+        min(root);
+    }
+
+    private int min(Node root) {
+        if (root == null) return -1;
+        if (isLeaf(root))
+            return root.value;
+        var left = min(root.leftChild);
+        var right = min(root.rightChild);
+        return Math.min(Math.min(left, right), root.value);
+    }
+
+    private boolean isLeaf(Node root) {
+        return root.leftChild == null && root.rightChild == null;
     }
 
     public void traverseInOrder() {
