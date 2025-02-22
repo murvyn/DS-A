3604e47bbd9f96e850203a3e882cab66d5a60a1d
Murvyn <marvin.asamoah.123@gmail.com>
2025-02-22 00:09:56 +0000

    implememnt autocompletion

--- TrieExercise.java
+++ TrieExercise.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class TrieExercise {
     public static void main(String[] args) {
@@ -6,11 +8,12 @@ public class TrieExercise {
         trie.insert("hello");
         trie.insert("world");
         trie.insert("apple");
-    } 
+    }
 }
 
 class Trie {
     public static int ALPHABET_SIZE = 26;
+
     private class Node {
         private final char value;
         // private Node[] children = new Node[ALPHABET_SIZE];
@@ -33,7 +36,7 @@ class Trie {
         public void addChild(char ch) {
             children.put(ch, new Node(ch));
         }
-        
+
         public Node getChild(char ch) {
             return children.get(ch);
         }
@@ -55,13 +58,13 @@ class Trie {
 
     public void insert(String value) {
         var current = root;
-        for(var ch : value.toCharArray()) {
+        for (var ch : value.toCharArray()) {
             // var index = ch - 'a';
             // if(current.children[index] == null) {
-            //     current.children[index] = new Node(ch);
+            // current.children[index] = new Node(ch);
             // }
             // current = current.children[index];
-            if(current.hasChild(ch)) {
+            if (current.hasChild(ch)) {
                 current.addChild(ch);
             }
             current = current.getChild(ch);
@@ -70,12 +73,12 @@ class Trie {
     }
 
     public boolean contains(String word) {
-        if(word == null || word.isEmpty() ){
+        if (word == null || word.isEmpty()) {
             return false;
         }
-        var current =root;
-        for(var ch: word.toCharArray()){
-            if(!current.hasChild(ch)){
+        var current = root;
+        for (var ch : word.toCharArray()) {
+            if (!current.hasChild(ch)) {
                 return false;
             }
             current = current.getChild(ch);
@@ -83,47 +86,81 @@ class Trie {
         return current.isEndOfWord;
     }
 
-    public void remove (String word) {
-        remove (root, word, 0);
+    public void remove(String word) {
+        remove(root, word, 0);
     }
 
-    private void remove (Node root, String word, int index) {
-        if(index == word.length()){
+    private void remove(Node root, String word, int index) {
+        if (index == word.length()) {
             root.isEndOfWord = false;
             return;
         }
-        var ch =  word.charAt(index);
+        var ch = word.charAt(index);
         var child = root.getChild(ch);
-        if(child == null){
+        if (child == null) {
             return;
         }
 
-        remove (child, word, index + 1);
+        remove(child, word, index + 1);
 
-        if(!child.hasChildren() && !child.isEndOfWord){
+        if (!child.hasChildren() && !child.isEndOfWord) {
             root.removeChild(ch);
         }
     }
 
-    public void preOrderTraverse () {
+    public List<String> findWords(String prefix) {
+        List<String> words = new ArrayList<>();
+        var lastNode = findLastNodeOf(prefix);
+        findWords(lastNode, words, prefix);
+        return words;
+    }
+
+    private void findWords(Node root, List<String> words, String prefix) {
+        if (root == null) {
+            return;
+        }
+        
+        if(root.isEndOfWord) {
+            words.add(prefix);
+        }
+
+        for (var child : root.getChildren()) {
+            findWords(child, words, prefix + child.value);
+        }
+    }
+
+    private Node findLastNodeOf(String prefix) {
+        var current = root;
+        for (var ch : prefix.toCharArray()) {
+
+            var child = current.getChild(ch);
+            if (child == null) {
+                return null;
+            }
+            current = child;
+        }
+        return current;
+    }
+
+    public void preOrderTraverse() {
         preOrderTraverse(root);
     }
 
-    private void preOrderTraverse (Node root) {
+    private void preOrderTraverse(Node root) {
         System.out.println(root.value);
-        for(var child : root.getChildren()) {
+        for (var child : root.getChildren()) {
             preOrderTraverse(child);
         }
-    } 
-   
-    public void postOrderTraverse () {
+    }
+
+    public void postOrderTraverse() {
         postOrderTraverse(root);
     }
 
-    private void postOrderTraverse (Node root) {
-        for(var child : root.getChildren()) {
+    private void postOrderTraverse(Node root) {
+        for (var child : root.getChildren()) {
             postOrderTraverse(child);
         }
         System.out.println(root.value);
-    } 
+    }
 }
