1d8ae0471616d8f4bc78607db267587f518d230f
Murvyn <marvin.asamoah.123@gmail.com>
2025-02-24 21:56:30 +0000

    implement graphs and implement addNodes and addEdge functions

---
+++ GraphExercise.java
@@ -0,0 +1,43 @@
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GraphExercise {
+    public static void main(String[] args) {
+
+    }
+}
+
+class Graph {
+    private class Node {
+        private String label;
+        public Node(String label) {
+            this.label = label;
+        }
+    }
+
+    private final Map<String, Node> nodes = new HashMap<>();
+    private final Map<Node, List<Node>> adjacentNodes = new HashMap<>();
+
+    public void addNode(String label) {
+        var node = new Node(label);
+        nodes.putIfAbsent(label, node);
+        adjacentNodes.putIfAbsent(node, new ArrayList<>());
+    } 
+
+    public void addEdge(String from, String to) {
+        var fromNode = nodes.get(from);
+        if (fromNode == null) {
+            throw new IllegalArgumentException();
+        }
+        
+        var toNode = nodes.get(from);
+        if (toNode == null) {
+            throw new IllegalArgumentException();
+        }
+
+        adjacentNodes.get(fromNode).add(toNode);
+    }
+}
