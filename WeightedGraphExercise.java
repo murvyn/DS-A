411758b71caa672636ec7946ae69efc953bd2c88
Murvyn <marvin.asamoah.123@gmail.com>
2025-02-27 22:24:54 +0000

    implement dijkstra shortest path algorithm

--- WeightedGraphExercise.java
+++ WeightedGraphExercise.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
 
 public class WeightedGraphExercise {
     public static void main(String[] args) {
@@ -15,11 +20,11 @@ public class WeightedGraphExercise {
     }
 }
 
-
 class WeightedGraph {
     private class Node {
         private final String label;
         private List<Edge> edges = new ArrayList<>();
+
         public Node(String label) {
             this.label = label;
         }
@@ -51,17 +56,17 @@ class WeightedGraph {
 
         @Override
         public String toString() {
-            return from + " -> " + to ;
+            return from + " -> " + to;
         }
     }
 
     private final Map<String, Node> nodes = new HashMap<>();
 
-    public void addNode (String label) {
+    public void addNode(String label) {
         nodes.putIfAbsent(label, new Node(label));
     }
 
-    public void addEdge (String from, String to, int weight ) {
+    public void addEdge(String from, String to, int weight) {
         var fromNode = nodes.get(from);
         var toNode = nodes.get(to);
         if (fromNode == null || toNode == null) {
@@ -75,10 +80,90 @@ class WeightedGraph {
     public void print() {
         for (var node : nodes.values()) {
             var edges = node.getEdges();
-            if(!edges.isEmpty()) {
+            if (!edges.isEmpty()) {
                 System.out.println(node + "is connected to " + edges);
             }
         }
     }
 
+    public class NodeEntry {
+        private final Node node;
+        private final int priority;
+
+        public NodeEntry(Node node, int priority) {
+            this.node = node;
+            this.priority = priority;
+        }
+    }
+
+    public Path getShortestPath(String from, String to) {
+        var fromNode = nodes.get(from);
+        var toNode = nodes.get(to);
+        if (fromNode == null || toNode == null) {
+            throw new IllegalArgumentException();
+        }
+        Map<Node, Integer> distance = new HashMap<>();
+        Set<Node> visited = new HashSet<>();
+        Map<Node, Node> previousNodes = new HashMap<>();
+        for (var node : nodes.values()) {
+            distance.put(node, Integer.MAX_VALUE);
+        }
+        distance.replace(fromNode, 0);
+
+        PriorityQueue<NodeEntry> queue = new PriorityQueue<>(Comparator.comparingInt(ne -> ne.priority));
+
+        queue.add(new NodeEntry(fromNode, 0));
+
+        while (!queue.isEmpty()) {
+            var current = queue.remove().node;
+            visited.add(current);
+
+            for (var edge : current.getEdges()) {
+                if (visited.contains(edge.to)) {
+                    continue;
+                }
+
+                var newDistance = distance.get(current) + edge.weight;
+                if (newDistance < distance.get(edge.to)) {
+                    distance.replace(edge.to, newDistance);
+                    previousNodes.put(edge.to, current);
+                    queue.add(new NodeEntry(edge.to, newDistance));
+                }
+            }
+        }
+
+        return buildPath(toNode, previousNodes);
+
+    }
+
+    private Path buildPath(WeightedGraph.Node toNode, Map<Node, Node> previousNodes) {
+        Stack<Node> stack = new Stack<>();
+        stack.push(toNode);
+        var previous = previousNodes.get(toNode);
+
+        while (previous != null) {
+            stack.push(previous);
+            previous = previousNodes.get(previous);
+        }
+
+        var path = new Path();
+        while (!stack.isEmpty()) {
+            path.add(stack.pop().label);
+        }
+        return path;
+    }
+
+}
+
+class Path {
+    private final List<String> nodes = new ArrayList<>();
+
+    public void add(String node) {
+        nodes.add(node);
+    }
+
+    @Override
+    public String toString() {
+        return nodes.toString();
+    }
 }
