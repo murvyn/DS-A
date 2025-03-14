fde7c3e97ce9016cdb5911c813bfb83dc5db48cf
Murvyn <marvin.asamoah.123@gmail.com>
2025-03-14 22:53:03 +0000

    implement word reversing

--- string manipulation algorithm/StringUtils.java
+++ string manipulation algorithm/StringUtils.java
@@ -1,4 +1,7 @@
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class StringUtils {
     public static int countVowels(String str) {
         if (str == null || str.isEmpty()) {
@@ -13,15 +16,30 @@ public class StringUtils {
         }
         return count;
     }
-    
+
     public static String reverse(String str) {
         if (str == null || str.isEmpty()) {
-            return str;
+            return "";
         }
         StringBuilder reversed = new StringBuilder();
-        for(int i = str.length() - 1; i >= 0; i--) {
+        for (int i = str.length() - 1; i >= 0; i--) {
             reversed.append(str.charAt(i));
         }
         return reversed.toString();
+    }
+
+    public static String reverseWords(String sentence) {
+        if (sentence == null || sentence.isEmpty()) {
+            return "";
+        }
+        String[] words = sentence.split("");
+        Collections.reverse(Arrays.asList(words));
+        return String.join(" ", words);
+
+        // StringBuilder reversed = new StringBuilder();
+        // for(int i = words.length - 1; i >= 0; i--) {
+        // reversed.append(words[i] + " ");
+        // }
+        // return reversed.toString().trim();
     }
 }
