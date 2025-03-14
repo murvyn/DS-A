e1897e848aa60183fdca72fcb0706279093f791c
Murvyn <marvin.asamoah.123@gmail.com>
2025-03-14 22:46:23 +0000

    implement string reversal

--- string manipulation algorithm/StringUtils.java
+++ string manipulation algorithm/StringUtils.java
@@ -13,4 +13,15 @@ public class StringUtils {
         }
         return count;
     }
+    
+    public static String reverse(String str) {
+        if (str == null || str.isEmpty()) {
+            return str;
+        }
+        StringBuilder reversed = new StringBuilder();
+        for(int i = str.length() - 1; i >= 0; i--) {
+            reversed.append(str.charAt(i));
+        }
+        return reversed.toString();
+    }
 }
